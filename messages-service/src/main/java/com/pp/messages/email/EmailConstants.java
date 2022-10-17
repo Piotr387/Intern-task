18fc67266e89cae32b98de368d8183fd9de1f651
Piotr387 <ptrpiw.dev@gmail.com>
2022-10-17 13:10:44 +0200

    Added sending email with Papercut SMTP

--- messages-service/src/main/java/com/pp/messages/email/EmailConstants.java
+++ messages-service/src/main/java/com/pp/messages/email/EmailConstants.java
@@ -12,20 +12,18 @@ import java.util.Date;
 final class EmailConstants {
 
     static String createConfirmationOfSigningUpForLecture(EmailSignUpConfirmationRequest request) {
-        String dateOfSend = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        String message = "Dziękujemy za rejestracje " + request.login() + ", " +
+        String message = "Dziękujemy za rejestracje " + request.login() + ", \n" +
                 "zostałeś pomyślnie zapisany na " + request.lectureName() + " " +
-                "start wydarzenia " + request.lectureStartingTime() + " pod koniec prelekcji " +
+                "\nStart wydarzenia " + request.lectureStartingTime() + " pod koniec prelekcji " +
                 "przewidziana przerwa na kawę";
-        return dateOfSend + ", " + request.email() + ", " + message + "\n";
+        return message + "\n";
     }
 
     public static String createConfirmationOfRegistration(EmailRegistrationConfirmRequest request) {
-        String dateOfSend = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-        String message = "Dziękujemy za rejestracje w naszym systemie " + request.login() + ", " +
-                "Od teraz zapisywać się na kolejne prelekcje możes z poziomu konta. " +
-                "Posiadając konto możesz zarządzać swoimi rezerwacjami. Twoje dane logowania: " +
+        String message = "Dziękujemy za rejestracje w naszym systemie " + request.login() + ", \n" +
+                "Od teraz zapisywać się na kolejne prelekcje możes z poziomu konta. \n" +
+                "Posiadając konto możesz zarządzać swoimi rezerwacjami. \nTwoje dane logowania: " +
                 "Login: " + request.login() + " hasło: " + request.password();
-        return dateOfSend + ", " + request.email() + ", " + message + "\n";
+        return message + "\n";
     }
 }
--- messages-service/src/main/java/com/pp/messages/email/EmailService.java
+++ messages-service/src/main/java/com/pp/messages/email/EmailService.java
@@ -1,21 +1,68 @@
 package com.pp.messages.email;
 
-import com.pp.messages.file.FileAssistant;
-import com.pp.messages.file.FileAssistantTxt;
 import com.pp.messages.EmailRegistrationConfirmRequest;
 import com.pp.messages.EmailSignUpConfirmationRequest;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 class EmailService {
 
+    private static final String FROM_WHO = "inser@email.com";
+    private static final String SUBJECT_INVITATION_EMAIL = "Pomyślna rejestracja";
+    private static final String SUBJECT_CONFIRMATION_EMAIL = "Pomyślne zapisanie sie na prelekcje";
+
     public void send(EmailSignUpConfirmationRequest request) {
-        FileAssistant fileAssistant = new FileAssistantTxt();
-        fileAssistant.writeToTheEndOfFile(EmailConstants.createConfirmationOfSigningUpForLecture(request));
+        var message = EmailConstants.createConfirmationOfSigningUpForLecture(request);
+        sendEmailViaLocalServer(request.email(), SUBJECT_CONFIRMATION_EMAIL, message);
     }
 
     public void sendInvitationEmail(EmailRegistrationConfirmRequest request){
-        FileAssistant fileAssistant = new FileAssistantTxt();
-        fileAssistant.writeToTheEndOfFile(EmailConstants.createConfirmationOfRegistration(request));
+        var message = EmailConstants.createConfirmationOfRegistration(request);
+        sendEmailViaLocalServer(request.email(), SUBJECT_INVITATION_EMAIL, message);
+    }
+
+    @SneakyThrows
+    private void sendEmailViaLocalServer(String email, String subject, String messageText){
+        // Assuming you are sending email from localhost
+        String host = "localhost";
+        // Assuming you are sending email from localhost.
+        String port = "25";
+        // Get system properties
+        Properties properties = System.getProperties();
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        // Setup mail server.
+        properties.setProperty("mail.smtp.port", port);
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(FROM_WHO));
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            // Set Subject: header field
+            message.setSubject(subject);
+            // Now set the actual message
+            message.setText(messageText);
+            // Send message
+            Transport.send(message);
+            log.info("Message sent successfully to {}", email);
+        }catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
 }
